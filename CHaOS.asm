d1735e0ca9777f5b2b6b57e369a210e887be4107
rickroller314 <43557713+rickroller314@users.noreply.github.com>
2022-06-23 19:38:04 -0400

    LS implemented
    Needs modifier to print actual numerical values in hex form

--- CHaOS.asm
+++ CHaOS.asm
@@ -1,4 +1,32 @@
 ; https://wchargin.com/lc3web
 
 .ORIG x0000 ; Starting place in memory for our code
+
+TRAP x01 ; jump to the OS
+.FILL x0200 ; where the OS starts
+.FILL x3000 ; where the user code starts
+.BLKW x1d
+.FILL x0400 ; GETC 
+.FILL x0430 ; OUT
+.FILL x0450 ; PUTS
+.FILL x04a0 ; IN
+.FILL x04e0 ; PUTSP
+.FILL xfd70 ; HALT
+.BLKW x1ea
+
+OS_START
+
+LS
+LEA r0, DIRECTORY
+OUTS
+TRAP x1
+
+MK
+LD r1, DIRECTORY_PTR
+LEA r0, DIRECTORY
+ADD r1, r0, r1
+DIRECTORY_PTR .FILL x0
+DIRECTORY .blkw x80
+
+
 .END
